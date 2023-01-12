2609db8614f62ced9cf0b7c03d73b57f5aa74549
dontife <dontife@ymail.com>
2023-01-11 19:27:34 -0500

    add logic to handle the player's choice

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // Write your JavaScript code here
+
+let playerChoice = "";
+let playerChoiceContainer = document.querySelector("#player-choice-container"); 
+playerChoiceContainer.addEventListener("click", handlePlayerChoice);
+
+function handlePlayerChoice(event) {
+    if (!event.target.classList.contains("emoji")) return;
+    playerChoice = event.target.textContent;
+    playerChoiceContainer.innerHTML = `<p class="emoji">${playerChoice}</p>`;
+}
