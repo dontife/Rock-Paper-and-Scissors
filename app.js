0a7c032b89146a76c531d9b0fe34f1cd7bc86139
dontife <dontife@ymail.com>
2023-01-11 19:33:39 -0500

    show an emoji on the page for the computer's choice

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // Write your JavaScript code here
 
 let playerChoice = "";
+let computerChoice = "";
+let emojis = ["✂️ ", "📄", "🪨"];
+let emojiShuffleElement = document.querySelector("#emoji-shuffle");
 let playerChoiceContainer = document.querySelector("#player-choice-container"); 
 playerChoiceContainer.addEventListener("click", handlePlayerChoice);
+shuffleEmojis();
 
 function handlePlayerChoice(event) {
     if (!event.target.classList.contains("emoji")) return;
     playerChoice = event.target.textContent;
-    playerChoiceContainer.innerHTML = `<p class="emoji">${playerChoice}</p>`;
+    playerChoiceContainer.innerHTML = `<p class="emoji">${playerChoice}</p>`;git 
+}
+function shuffleEmojis() {
+    computerChoice = emojis[1];
+    emojiShuffleElement.textContent = computerChoice;
+
 }
